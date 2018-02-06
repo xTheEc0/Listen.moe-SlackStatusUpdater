48a682e9231e4aa8e457d42855b74fcd9652cabe
Evaldas Lavrinovičius <lavrinovicius.evaldas@gmail.com>
2018-02-06 15:34:31 +0200

    Add discord status (presence) support

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 require('console-stamp')(console, {
     pattern: 'HH:MM:ss',
     colors: {
-        stamp: ["white", "bgRed"],
-        label: "white"
-    }
+        stamp: ['white', 'bgRed'],
+        label: 'white',
+    },
 });
 require('dotenv').config({
-    path: `${__dirname}/.env`
+    path: `${__dirname}/.env`,
 });
 
 const Discord = require('discord.js');
@@ -14,29 +14,36 @@ const request = require('request-promise-native');
 const ListenMoeJS = require('listenmoe.js');
 
 const discordClient = new Discord.Client();
+let discordReady = false;
 const moe = new ListenMoeJS();
 
 process.env.DISCORD_TOKEN.split(',').map(token => {
     discordClient.login(token);
 });
 
+discordClient.on('ready', () => {
+    console.log(`Logged in as ${discordClient.user.tag}!`);
+    discordReady = true;
+});
+
 moe.on('updateTrack', data => {
-    let songInfo = data.song;
+    const songInfo = data.song;
     logDebugMessage(songInfo);
 
-    let artists = getArtists(songInfo.artists);
+    const artists = getArtists(songInfo.artists);
 
-    let source = getSources(songInfo.sources);
+    const source = getSources(songInfo.sources);
 
     let nowPlaying = `${artists} - ${songInfo.title}`;
 
-    if (source)
+    if (source) {
         nowPlaying += ` [From Anime: ${source}]`;
+    }
 
     // Slack has a limit of 100 characters for the status message
     if (nowPlaying.length > 100) {
         nowPlaying = nowPlaying.substr(0, 98);
-        nowPlaying += `..`;
+        nowPlaying += '..';
     }
 
     updateSlack(nowPlaying);
@@ -57,38 +64,58 @@ function updateSlack(currentSong) {
             form: {
                 token: token,
                 profile: JSON.stringify({
-                    "status_text": currentSong,
-                    "status_emoji": ":listen-moe:"
-                })
-            }
-        })
+                    'status_text': currentSong,
+                    'status_emoji': ':listen-moe:',
+                }),
+            },
+        });
     }))
+        .then(() => {
+            console.log('Slack update: OK');
+        });
 }
 
 function updateDiscord(currentSong) {
-    discordClient.on('ready', () => {
+    console.log('We gon update');
+    if (discordReady) {
         discordClient.user.setPresence({
             game: {
                 name: currentSong,
-                type: "LISTENING"
-            }
-        });
-    })
+                type: 'LISTENING',
+            },
+        })
+            .then(() => {
+                console.log('Discord update: OK');
+            });
+
+    }
+
+    // discordClient.on('ready', () => {
+    // }
+    // .then(( => {
+    //     discordClient.user.setPresence({
+    //         game: {
+    //             name: currentSong,
+    //             type: 'LISTENING',
+    //         },
+    // });
+    // console.log('Discord update: OK');
+    // })
 }
 
 function getArtists(artists) {
-    var result = [];
-    artists.map(function (artist) {
-        var jointName = (artist.name ? artist.name : '') + (artist.nameRomaji ? (artist.name ? ' (' : '') + artist.nameRomaji + (artist.name ? ')' : '') : '');
+    const result = [];
+    artists.map(artist => {
+        const jointName = (artist.name ? artist.name : '') + (artist.nameRomaji ? (artist.name ? ' (' : '') + artist.nameRomaji + (artist.name ? ')' : '') : '');
         if (jointName !== '') result.push(jointName);
     });
     return result.join(', ');
 }
 
 function getSources(sources) {
-    var result = [];
-    sources.map(function (source) {
-        var jointName = (source.nameRomaji ? source.nameRomaji : '') + (source.name ? (source.nameRomaji ? ' (' : '') + source.name + (source.nameRomaji ? ')' : '') : '');
+    const result = [];
+    sources.map(source => {
+        const jointName = (source.nameRomaji ? source.nameRomaji : '') + (source.name ? (source.nameRomaji ? ' (' : '') + source.name + (source.nameRomaji ? ')' : '') : '');
         if (jointName !== '') result.push(jointName);
     });
     return result.join(', ');
